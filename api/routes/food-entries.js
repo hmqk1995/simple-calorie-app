c6e681cefb8a03484ac8c4f4d29e7ea31a778468
Kun Qian <qiankun1995@gmail.com>
2022-08-02 21:50:31 -1000

    feat: initial commit

--- api/routes/food-entries.js
+++ api/routes/food-entries.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
+const { authenticateToken } = require('../services/users');
+
 // POST: add a new food entry
 router.post('/food-enrties', (req, res) => {
   res.send('Add a new food entry');
 })
 
 // GET: get all food entries
-router.get('/food-enrties', (req, res) => {
+router.get('/food-enrties', authenticateToken, (req, res) => {
   res.send('Get all food entries');
 })
 
 // GET: get single food entry
 router.get('/food-enrties/:foodId', (req, res) => {
-  res.send('Get all food entries');
+  res.send('Get a single food entry');
 })
 
 module.exports = router;
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const { generateAccessToken } = require('../services/users');
 
 // get all users
 router.get('/users', (req, res) => {
   res.send('Get all users');
-})
+});
 
 // Get single user
 router.get('/users/:username', (req, res) => {
   res.send('Get a single user');
-})
+});
 
-// auth the user with token
-router.get('/users/:username/auth', (req, res) => {
-  res.send('auth the user with token');
-})
-
+// send authtoken to the user
+router.post('/users/:username/auth', (req, res) => {
+  const token = generateAccessToken({ username: req.params.username });
+  res.json(token);
+});
 
 module.exports = router;
